f76be680e871358ffb01a3b51c52f0500d78bb96
Eric Majerus <issmichnicht@gmail.com>
2017-06-24 14:40:39 -0500

    replaced var with const

--- .junk/rss-test.js
+++ .junk/rss-test.js
@@ -1,16 +1,16 @@
-var FeedParser = require('feedparser')
-var request = require('request') // for fetching the feed
-var cheerio = require('cheerio')
+const FeedParser = require('feedparser')
+const request = require('request') // for fetching the feed
+const cheerio = require('cheerio')
 
-var req = request('http://comicfeeds.chrisbenard.net/view/dilbert/default')
-var feedparser = new FeedParser()
+const req = request('http://comicfeeds.chrisbenard.net/view/dilbert/default')
+const feedparser = new FeedParser()
 
 req.on('error', function (error) {
     // handle any request errors
 })
 
 req.on('response', function (res) {
-    var stream = this // `this` is `req`, which is a stream
+    const stream = this // `this` is `req`, which is a stream
 
     if (res.statusCode !== 200) {
         this.emit('error', new Error('Bad status code'))
@@ -26,9 +26,9 @@ feedparser.on('error', function (error) {
 
 feedparser.on('readable', function () {
     // This is where the action is!
-    var stream = this // `this` is `feedparser`, which is a stream
-    var meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
-    var item
+    const stream = this // `this` is `feedparser`, which is a stream
+    const meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
+    let item
 
     while (item = stream.read()) {
         const $ = cheerio.load(item.description)
