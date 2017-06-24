86e4e487dfd0889750d1a4376fd968f8d24307e1
Eric Majerus <issmichnicht@gmail.com>
2017-06-23 20:37:03 -0500

    Added node/express for api calls. For now runs on separate port because I don't know how to configure it with webpack

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const lessMiddleware = require('less-middleware')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(lessMiddleware(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//----------------//----------------//----------------//----------------//----------------
+// App Error Handlers
+//----------------//----------------//----------------//----------------//----------------
+//Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500)
+        res.send({
+            message: err.message,
+            error: err
+        })
+    })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.send({
+        message: err.message,
+        error: {}
+    })
+})
+
+module.exports = app
